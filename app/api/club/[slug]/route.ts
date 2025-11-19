b1dcb29d1d2c9ab3413ef4904c298559f3e441c9
Florian Fougeray <fougeray.florian@gmail.com>
2025-11-19 17:30:47 +0100

    Route to get config et res data to front ok

--- app/api/club/[slug]/route.ts
+++ app/api/club/[slug]/route.ts
@@ -1,11 +1,26 @@
-import { getClubConfig } from "@/lib/getClubConfig";
+import { NextRequest } from "next/server";
+import { fetchRemoteConfigForClub } from "@/lib/unity/club.controller";
 
-type PropsRoute = {
+type Props = {
   params: Promise<{ slug: string }>;
 };
 
-export async function GET(request: Request, { params }: PropsRoute) {
-  const { slug } = await params;
-  const result = await getClubConfig(slug);
-  return new Response(result);
+// ----------------------------------------------------- //
+// ROUTE GET CONFIG BY NAME AND RETURN TO FRONT
+// ----------------------------------------------------- //
+export async function GET(req: NextRequest, { params }: Props) {
+  try {
+    const { slug } = await params;
+
+    const remote = await fetchRemoteConfigForClub(slug);
+    if (!remote) {
+      return Response.json({ error: "Remote config not found" }, { status: 404 });
+    }
+
+    const settings = remote?.configs?.settings;
+    return Response.json(settings);
+  } catch (error) {
+    console.log("Route GET /club/[slug] error:", error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
