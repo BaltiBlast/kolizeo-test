f33bd12d208e6ad053b413660b2eec9f9b251c2c
Florian Fougeray <fougeray.florian@gmail.com>
2025-11-19 19:05:12 +0100

    Adding links on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Choisis un club</h1>
-      <Link href="/club/fcmetz">FC Metz</Link>
-      <br />
-      <Link href="/club/metzhandball">Metz Handball</Link>
-    </main>
+    <section className="landing">
+      <h2>Choisi le club de ton choix : </h2>
+      <ul>
+        <li>
+          <Link href="/club/fcmetz">FC Metz</Link>
+        </li>
+        <li>
+          <Link href="/club/metzhandball">Metz Handball</Link>
+        </li>
+      </ul>
+    </section>
   );
 }
