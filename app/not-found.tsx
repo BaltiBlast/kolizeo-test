0183c7f60bd6ba700461a47c7899456133b129e5
Florian Fougeray <fougeray.florian@gmail.com>
2025-11-19 18:36:29 +0100

    Adding 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <section className="not-found">
+      <h1>404 - Page introuvable</h1>
+      <p>La page que tu cherches n'existe pas.</p>
+
+      <a href="/">Retour à l’accueil</a>
+    </section>
+  );
+}
